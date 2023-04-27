36d4c4c7130551718dde7080311596bc36dfae58
Caroline Cahilly <carolinecahilly@DN0a24a144.SUNet>
2023-04-26 20:59:27 -0700

    Modified to loop through all images & usability

--- mvs_objaverse-main/render_all_models_comparisons.py
+++ mvs_objaverse-main/render_all_models_comparisons.py
@@ -1,52 +1,72 @@
+'''
+Renders all of the data in the folder_assets with eevee and cycles.
+'''
+
 import os
 import argparse
 
+user = "Caroline"
+
+if user == "Daniel":
+    out_path = '/Users/jaeyounglee/Desktop/CS231N_Final_Project'
+    in_path = '/Users/jaeyounglee/Desktop/CS231N_Final_Project'
+    blender_path = '/Applications/Blender.app/Contents/MacOS/Blender'
+
+elif user == "Caroline":
+    out_path = '/Users/carolinecahilly/Desktop/mvs_objaverse-main/output'
+    in_path = '/Users/carolinecahilly/.objaverse/hf-objaverse-v1/glbs/'
+    blender_loc = '/Applications/Blender.app/Contents/MacOS/Blender'
+
 parser = argparse.ArgumentParser(description='Renders glbs')
 parser.add_argument(
-    '--save_folder', type=str, default='/Users/jaeyounglee/Desktop/CS231N_Final_Project',
+    '--save_folder', type=str, default=out_path,
     help='path for saving rendered image')
 parser.add_argument(
-    '--folder_assets', type=str, default='/Users/jaeyounglee/.objaverse/hf-objaverse-v1/glbs',
+    '--folder_assets', type=str, default=in_path,
     help='path to downloaded 3d assets')
 parser.add_argument(
-    '--blender_root', type=str, default='/Applications/Blender.app/Contents/MacOS/Blender',
+    '--blender_root', type=str, default=blender_path,
     help='path to blender executable')
 opt = parser.parse_args()
 
 
-
 # get all the file
 import glob 
-data = sorted(glob.glob(f"{opt.folder_assets}/*/"))[10:]
 
-for path in data:
-    # path = data[-5]
-    path = sorted(glob.glob(path + "/*.glb"))[0]
+all_data = sorted(glob.glob(f"{opt.folder_assets}/*/"))
 
-    # render_cmd = '%s -b -P rendering/render_blender.py -- --obj %s --output %s --views 100 --resolution 400' % (
-    #     opt.blender_root, path, opt.save_folder
-    # )
+for obj in range(len(all_data)):
 
-    # print(render_cmd)
-    # os.system(render_cmd)
+    data = all_data[obj:]
+    
+    for path in data:
+        # path = data[-5]
+        path = sorted(glob.glob(path + "/*.glb"))[0]
+
+        # render_cmd = '%s -b -P rendering/render_blender.py -- --obj %s --output %s --views 100 --resolution 400' % (
+        #     opt.blender_root, path, opt.save_folder
+        # )
+
+        # print(render_cmd)
+        # os.system(render_cmd)
 
 
-    os.makedirs(opt.save_folder + "eevee",exist_ok=True)
-    render_cmd = '%s -b -P rendering/render_blender.py -- --obj %s --output %s --views 10 --resolution 400 --add_floor --engine BLENDER_EEVEE' % (
-        opt.blender_root, path, opt.save_folder + "eevee"
-    )
-    print(render_cmd)
-    os.system(render_cmd)
-    print("EEVEE DONE")
+        os.makedirs(opt.save_folder + "_eevee_" + str(obj),exist_ok=True)
+        render_cmd = '%s -b -P rendering/render_blender.py -- --obj %s --output %s --views 10 --resolution 400 --add_floor --engine BLENDER_EEVEE' % (
+            opt.blender_root, path, opt.save_folder + "_eevee_" + str(obj)
+        )
+        print(render_cmd)
+        os.system(render_cmd)
+        print("EEVEE DONE")
 
-    os.makedirs(opt.save_folder + "cycles",exist_ok=True)
-    render_cmd = '%s -b -P rendering/render_blender.py -- --obj %s --output %s --views 10 --resolution 400 --add_floor --engine CYCLES' % (
-        opt.blender_root, path, opt.save_folder + "cycles"
-    )
-    print(render_cmd)
-    os.system(render_cmd)
-    print("CYCLES DONE")
-    # render_cmd = '%s -b -P rendering/render_blender.py -- --obj %s --output %s --views 100 --depth --resolution 400 > tmp.out' % (
-    #     opt.blender_root, path, opt.save_folder
-    # )
-    break
+        os.makedirs(opt.save_folder + "_cycles_" + str(obj),exist_ok=True)
+        render_cmd = '%s -b -P rendering/render_blender.py -- --obj %s --output %s --views 10 --resolution 400 --add_floor --engine CYCLES' % (
+            opt.blender_root, path, opt.save_folder + "_cycles_" + str(obj)
+        )
+        print(render_cmd)
+        os.system(render_cmd)
+        print("CYCLES DONE")
+        # render_cmd = '%s -b -P rendering/render_blender.py -- --obj %s --output %s --views 100 --depth --resolution 400 > tmp.out' % (
+        #     opt.blender_root, path, opt.save_folder
+        # )
+        break
